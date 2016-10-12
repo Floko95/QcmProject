d92fb0ba69fc427c3df851baa848e571cd31fdd6
Floko95 <flo.kot@free.fr>
2016-10-12 17:18:25 +0200

    Importer

--- Projet/Importer.php
+++ Projet/Importer.php
@@ -22,9 +22,13 @@
 </form>
 <?php
 $bdd=[
-'Informatique'=>['BDD'=>['Q1bdd','Q2bdd','Q3bdd'],'PHP'=>['Q1php','Q2php','Q3php']],
+'Informatique'=>['BDD'=>['Q1bdd','Q2bdd','Q3bdd'],'PHP'=>['Q1php','Q2php','Q3php'],'JAVA'=>['Q1java','Q2java','Q3java'],'C'['Q1c','Q2c','Q3c'],'HTML'=>['Q1html','Q2html','Q3html']],
 'Mathematiques'=>['Geométrie'=>['Q1geo','Q2geo','Q3geo'],'Algèbre'=>['Q1algo','Q2algo','Q3algo']],
-'Culture  Generale'=>['Histoire'=>['Q1his','Q2hist','Q3hist'],'Cinéma'=>['Q1cin','Q2cin','Q3cin']]
+'Culture  Generale'=>['Histoire'=>['Q1his','Q2hist','Q3hist'],'Cinéma'=>['Q1cin','Q2cin','Q3cin']],
+'Medecine'=>['Neurologie'=>['Q1neuro','Q2neuro','Q3neuro'],'Chirurgie'=>['Q1chi','Q2chi','Q3chi']],
+'Langues'=>['Anglais'=>['Q1ang','Q2ang','Q3ang'],'Espagnol'=>['Q1esp','Q2esp','Q3esp']],
+'Physique-chimie'=>['Physique'=>['Q1phys','Q2phys','Q3phys'],'Chimie'=>['Q1chim','Q2chim','Q3chim']]
+
 ];
  if(isset($_GET['domaine']) and isset($_GET['sdomaine']) and trim($_GET['domaine']!='') and trim($_GET['sdomaine']!=''))
 {
