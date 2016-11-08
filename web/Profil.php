6d31bc22c07842257e434a6d4f62062184c5302f
Floko95 <flo.kot@free.fr>
2016-11-08 21:00:30 +0100

    Profil

--- web/Profil.php
+++ web/Profil.php
@@ -12,7 +12,6 @@
 <?php 
 session_start();
 try{
-$bdd=new PDO('pgsql:host=localhost;dbname=postgres','postgres','password');
 $bdd->query('SET NAMES utf8');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
@@ -28,31 +27,32 @@ if (isset($_POST['qcmb']) and trim($_POST['qcmb']!=''))
 	$req->execute();
 	while($ligne=$req->fetch(PDO::FETCH_ASSOC))
 {
-	echo "Question: $ligne['question']<br/>";
+	echo 'Question: '.$ligne['question'].'<br/>';
 	
 }
-echo ' <form action="Profil.php" method="post"><input type="hidden" name="id" value="'.$_POST['id']'" /><input type="submit" name="supp" value="Supprimer le QCM" /></form>';
+echo ' <form action="Profil.php" method="post"><input type="hidden" name="id" value="'.$_POST['id'].'" /><input type="submit" name="supp" value="Supprimer le QCM" /></form>';
 }
 
 elseif (isset($_POST['supp']) and trim($_POST['supp']!=''))
 {
 	echo ' <p>Voulez vous vraiment supprimer le QCM numéro '.$_POST['id'].'?</p>';
-	echo ' <form action="Profil.php" method="post"><input type="hidden" name="id" value="'.$_POST['id']'" /><input type="submit" name="suppc" value="Oui"/> <input type="submit" name="suppn" value="Non"/></form>'
+	echo ' <form action="Profil.php" method="post"><input type="hidden" name="id" value="'.$_POST['id'].'" /><input type="submit" name="suppc" value="Oui"/> <input type="submit" name="suppn" value="Non"/></form>';
 	
 }
 elseif(isset($_POST['suppc']))
 {
-	$req=$bdd->prepare("DELETE * FROM public.qcm_question WHERE public.qcm_question.id_qcm=:id");
+	
+	$req=$bdd->prepare("DELETE  FROM public.qcm_question WHERE id_qcm=:id");
 	$req->bindValue(':id',$_POST['id']);
 	$req->execute();
-	$req=$bdd->prepare("DELETE * FROM public.qcm WHERE public.qcm.id_qcm=:id");
+	$req=$bdd->prepare("DELETE  FROM public.qcm WHERE public.qcm.id_qcm=:id");
 	$req->bindValue(':id',$_POST['id']);
 	$req->execute();
 	echo ' <p> Le questionnaire numéro '.$_POST['id']. ' a été supprimé.</p><br/><a href=Profil.php>retour</a>';
 }
 elseif(isset($_POST['suppn']))
 {
-	header('Location:Profil.php');
+	header('Location: Profil.php');
 }
 else
 {
@@ -61,7 +61,7 @@ $req->bindValue(':aut',$_SESSION['user']);
 $req->execute();
 while($ligne=$req->fetch(PDO::FETCH_ASSOC))
 {
-	echo '<form action="Profil.php" method="post"><input type="submit" name="qcmb" value="QCM numéro '.$ligne['id_qcm'].'" /><input type="hidden" name="id" value="'</form>.$ligne['id_qcm'].'" /></form>';
+	echo '<form action="Profil.php" method="post"><input type="submit" name="qcmb" value="QCM numéro '.$ligne['id_qcm'].'" /><input type="hidden" name="id" value="'.$ligne['id_qcm'].'" /></form>';
 	
 }
 }
