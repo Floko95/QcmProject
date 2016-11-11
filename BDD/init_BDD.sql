428945f8f894d6fb96fc8b141b149848d1ea179c
Xylllom <xylllom@ymail.com>
2016-11-11 23:06:56 +0100

    Ajout des réponses et modifs mineures

---
+++ BDD/init_BDD.sql
@@ -0,0 +1,84 @@
+
+/* Script d'initialisation de la BDD, a utiliser une seule fois */
+
+CREATE SEQUENCE IF NOT EXISTS ID_DOMAINE START 1 INCREMENT 1 MAXVALUE 80;
+CREATE SEQUENCE IF NOT EXISTS ID_SOUS_DOMAINE START 1 INCREMENT 1 MAXVALUE 80;
+CREATE SEQUENCE IF NOT EXISTS ID_QUESTIONNEUR START 1 INCREMENT 1 MAXVALUE 100;
+CREATE SEQUENCE IF NOT EXISTS ID_REPONDEUR START 1 INCREMENT 1 MAXVALUE 300;
+CREATE SEQUENCE IF NOT EXISTS ID_QUESTION START 1 INCREMENT 1 MAXVALUE 800;
+CREATE SEQUENCE IF NOT EXISTS ID_REPONSE START 1 INCREMENT 1 MAXVALUE 1200;
+CREATE SEQUENCE IF NOT EXISTS ID_QCM START 1 INCREMENT 1 MAXVALUE 400;
+
+
+CREATE TABLE IF NOT EXISTS Domaine(
+id_domaine integer PRIMARY KEY DEFAULT nextval('ID_DOMAINE'),
+nom_domaine varchar NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Sous_Domaine(
+id_sous_domaine integer PRIMARY KEY DEFAULT nextval('ID_SOUS_DOMAINE'),
+id_domaine integer REFERENCES Domaine(id_domaine),
+nom_sous_domaine varchar NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Questionneur(
+id_questionneur integer PRIMARY KEY DEFAULT nextval('ID_QUESTIONNEUR'),
+nom_questionneur varchar NOT NULL UNIQUE,
+mdp_questionneur varchar NOT NULL,
+nb_qcm integer DEFAULT 0,
+UNIQUE (id_questionneur, nom_questionneur, mdp_questionneur)
+);
+
+CREATE TABLE IF NOT EXISTS Repondeur(
+id_repondeur integer PRIMARY KEY DEFAULT nextval('ID_REPONDEUR'), 
+nom_repondeur varchar NOT NULL UNIQUE,
+mdp_repondeur varchar NOT NULL,
+UNIQUE (id_repondeur, nom_repondeur, mdp_repondeur)
+);
+
+CREATE TABLE IF NOT EXISTS Question(
+id_question integer PRIMARY KEY DEFAULT nextval('ID_QUESTION'),
+question varchar NOT NULL,
+valeur float NOT NULL DEFAULT 1.0, -- points gagnés lors d'une bonne réponse à la question
+temps integer NOT NULL DEFAULT 30, -- champs de durée en secondes 
+UNIQUE (question)
+);
+
+CREATE TABLE IF NOT EXISTS Reponse(
+id_reponse integer PRIMARY KEY DEFAULT nextval('ID_REPONSE'),
+id_question integer NOT NULL REFERENCES Question(id_question),
+reponse varchar NOT NULL,
+correct boolean DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS Qcm(
+id_qcm integer NOT NULL PRIMARY KEY DEFAULT nextval('ID_QCM'),
+auteur varchar NOT NULL REFERENCES Questionneur(nom_questionneur),
+date_creation date,
+niveau varchar,
+note float
+);
+
+CREATE TABLE IF NOT EXISTS QCM_Question(
+id_qcm integer REFERENCES Qcm(id_qcm),
+id_question integer REFERENCES Question(id_question),
+domaine varchar REFERENCES Domaine(nom_domaine),
+sous_domaine varchar REFERENCES Sous_Domaine(nom_sous_domaine),
+PRIMARY KEY(id_qcm, id_question)
+);
+
+/*CREATE TABLE IF NOT EXISTS Question_Reponse(
+id_question integer REFERENCES Question(id_question),
+id_reponse integer REFERENCES Reponse(id_reponse),
+PRIMARY KEY(id_question, id_reponse)
+);*/
+
+CREATE TABLE IF NOT EXISTS Recapitulatif(
+id_utilisateur integer NOT NULL REFERENCES Repondeur(id_repondeur),
+utilisateur varchar REFERENCES Repondeur(nom_repondeur),
+moyenne float,
+nb_qcm_fait integer,
+note_dernier_qcm float,
+temps_passe float,
+PRIMARY KEY (id_utilisateur)
+);
---
+++ BDD/insert_BDD.sql
@@ -0,0 +1,88 @@
+INSERT INTO questionneur (nom_questionneur, mdp_questionneur ) values
+('quest','quest'); -- questionneur test
+
+INSERT INTO repondeur (nom_repondeur, mdp_repondeur) values
+('rep','rep');  -- répondeur test
+
+INSERT INTO domaine (nom_domaine) values
+('Informatique'),('Mathématiques'),('Culture Générale'),('Médecine'),('Langues'),('Physique Chimie');
+
+INSERT INTO sous_domaine (id_domaine, nom_sous_domaine) values
+(1,'BDD'),(1,'PHP'),(1,'JAVA'),(1,'C'),(1,'HTML'),
+(2,'Algèbre'),(2,'Géometrie'),(3,'Histoire'),(3,'Cinéma'),
+(4,'Neurologie'),(4,'Chirurgie'),(5,'Anglais'),(5,'Espagnol'),
+(6,'Physique'),(6,'Chimie');
+
+INSERT INTO question (question) values
+('Qu''est ce qu''une clef étrangère ?'),('Combien y a-t-il de formes normales en SQL ?'),('Quelle instruction produit une erreur de syntaxe ?'),
+
+/*('Qu''est ce que l''informatique ?'),('Comment fonctionne un processeur ?'),('Comment fonctionne un serveur ?'),  ceci est bullshit  */
+
+('Quelles sont les balises pour utiliser le php ?'),('Peut-on exécuter du code php depuis un navigateur seul?'),('Que signifie l''acronyme PHP ?'),
+('Quelle la principale différence entre le Java et le C ?'),('De combien de Classe peut-on hériter ?'),('Qu''est ce qu''une interface ?'),
+('Quel est l''instruction de retour par défaut ?'),('Existe t-il un type boolean en langage C ?'),('A quoi sert le symbole * ?'),
+('A quoi sert le langage HTML en informatique ?'),('Avec quel autre langage utilise t-on très souvent l'' HTML ?'),('Comment reconnait-on du code html ?'),
+('Quel est le nom de l''algèbre qui se base sur la logique ?'),('Quels sont les trois opérateurs logiques de base ?'),('Quel algorithme permet de résoudre un système à 3 équations ?'),
+('Comment appelle t-on un objet avec 7 faces ?'),('Combien d''arrêtes possède un cube ?'),('Si deux plans sont sécants, comment appelle-t-on leur intersection ?'),
+('Quand a eu lieu la bataille de Waterloo ?'),('Quel est le vrai nom de l''empereur romain Auguste ?'),('Quelle est la date précise du début de la 2ème guerre mondiale pour la France ?'),
+('Dans quel film DiCaprio remporte-t-il son premier Oscar ?'),('Quel est le film le plus rentable de l''Histoire du cinéma ?'),('Quel est le premier long-métrage de Pixar créé uniquement par animation 3D ?'),
+('Q1neuro'),('Q2neuro'),('Q3neuro'), 	-- ne pas faire
+('Q1chir'),('Q2chir'),('Q3chir'),	-- ne pas faire
+('Q1ang'),('Q2ang'),('Q3ang'),		-- ne pas faire
+('Q1esp'),('Q2esp'),('Q3esp'),		-- ne pas faire
+('Q1phys'),('Q2phys'),('Q3phys'),	-- ne pas faire
+('Q1chim'),('Q2chim'),('Q3chim');	-- ne pas faire
+
+INSERT INTO reponse (id_question, reponse, correct) values
+(1,'Une valeur qui détermine l''unicité de chaque tuple',DEFAULT),(1,'Une valeur qui permet de lier une relation à une autre', TRUE),(1,'Une valeur qui permet d''accéder au rôle d''admin',DEFAULT),(1,'Je ne sais pas',DEFAULT),
+(2,'1', DEFAULT),(2,'2', DEFAULT),(2,'3', TRUE),(2,'4', DEFAULT),
+(3,'drop <table>',DEFAULT),(3,'select * from <table>',DEFAULT),(3,'delete * from <table>', TRUE),(3,'create <table>', DEFAULT),
+(4,'<php> </php>', DEFAULT),(4,'<?php ?>', TRUE),(4,'<php >', DEFAULT),(4,'<!php !>', DEFAULT),
+(5,'Oui', DEFAULT),(5,'Seulement si on est connecté à Internet', DEFAULT),(5,'Seulement si le navigateur supporte le php', DEFAULT),(5,'Non', TRUE),
+(6,'Physical HTML Processor', DEFAULT),(6,'Hypertext Pre Processor', TRUE),(6,'Hyperserver Post Proxy', DEFAULT),(6,'Processus Host PostgreSQL', DEFAULT),
+(7,'Le C est plus utilisé que le Java', DEFAULT),(7,'Le Java sait mieux gérer les ressources systèmes que le C', DEFAULT),(7,'Le Java est un langage objet mais pas le C', TRUE),(7,'Le C possède sa propre machine virtuelle mais pas Java', DEFAULT),
+(8,'On ne peut pas utiliser l''héritage en Java', DEFAULT),(8,'1 seule', TRUE),(8,'3 au maximum', DEFAULT),(8,'Autant que l''on souhaite', DEFAULT),
+(9,'C''est la manière dont est affichée le code', DEFAULT),(9,'Représente un contrat que doit remplir l''objet implémentant cette interface', TRUE),(9,'Permet d''effectuer un échange de données de manière invisible pour l''utilisateur', DEFAULT),(9,'Montre à quel point le Java est utile', DEFAULT),
+(10,'return 0', TRUE),(10,'return 1', DEFAULT),(10,'exit 0', DEFAULT),(10,'exit 1', DEFAULT),
+(11,'Non', TRUE),(11,'Non mais il existe bool', DEFAULT),(11,'Oui mais on ne l''utilise jamais', DEFAULT),(11,'Oui', DEFAULT),
+(12,'Permet d''ignorer le type d''une variable', DEFAULT),(12,'C''est la marque de fabrique des pointeurs en C', TRUE),(12,'On l''utilise uniquement pour instancier des tableaux', DEFAULT),(12,'Permet d''ignorer le nom d''une variable', DEFAULT),
+(13,'C''est utilisé pour la modélisation objet', DEFAULT),(13,'C''est utilisé pour la conception objet', DEFAULT),(13,'C''est utilisé pour la conception web', TRUE),(13,'C''est utilisé pour la modélisation web', DEFAULT),
+(14,'Le CSS', TRUE),(14,'Le C sharp', DEFAULT),(14,'Le Python', DEFAULT),(14,'L''UML', DEFAULT),
+(15,'On ne peut le comprendre à la première lecture', DEFAULT),(15,'On utilise que des balises', TRUE),(15,'On utilise que des pointeurs', DEFAULT),(15,'On doit le lire de droite à gauche', DEFAULT),
+(16,'Descartes', DEFAULT),(16,'Pascal', DEFAULT),(16,'Bool', TRUE),(16,'Gauss', DEFAULT),
+(17,'PLUS MOINS EGALE', DEFAULT),(17,'ET OU NON', TRUE),(17,'MULTIPLIE DIVISE RACINE', DEFAULT),(17,'Il n''y en a pas que 3', DEFAULT),
+(18,'Algorithme Euclide', DEFAULT),(18,'Algorithme Gauss', TRUE),(18,'Algorithme de Pythagore', DEFAULT),(18,'Algorithme RSA', DEFAULT),
+(19,'Septaèdre', DEFAULT),(19,'Heptaèdre', TRUE),(19,'Heptoèdre', DEFAULT),(19,'Septoèdre', DEFAULT),
+(20,'10', DEFAULT),(20,'12', TRUE ),(20,'14', DEFAULT),(20,'Il n''y a pas d''arrêtes dans un cube', DEFAULT),
+(21,'Un autre plan', DEFAULT),(21,'Une droite', TRUE),(21,'Un segment', DEFAULT),(21,'Un point', DEFAULT),
+(22,'1615', DEFAULT),(22,'1715', DEFAULT),(22,'1815', TRUE),(23,'1915', DEFAULT),
+(23,'Augustin', DEFAULT),(23,'Hector', DEFAULT),(23,'Octave', TRUE),(23,'Spartacus', DEFAULT),
+(24,'1 septembre 1939', DEFAULT),(24,'2 septembre 1939', DEFAULT),(24,'3 septembre 1939', TRUE),(24,'4 septembre 1939', DEFAULT),
+(25,'Titanic', DEFAULT),(25,'Le Loup de Wall Street', DEFAULT),(25,'Inception', DEFAULT),(25,'The Revenant', TRUE),
+(26,'Avatar', DEFAULT),(26,'Le Monde de Nemo', DEFAULT),(26,'Autant on emporte le vent', TRUE),(26,'Star Wars IV', DEFAULT),
+(27,'Les Indestructibles', DEFAULT),(27,'1001 pattes', DEFAULT),(27,'Toy Story', TRUE),(27,'Monstres et cie', DEFAULT);
+
+INSERT INTO qcm (auteur, date_creation, niveau, note) values
+('quest','18/10/2016','facile',20),
+('quest','05/11/2016','facile',20),
+('quest','05/11/2016','facile',20),
+('quest','05/11/2016','facile',20),
+('quest','05/11/2016','facile',20),
+('quest','05/11/2016','facile',20),
+('quest','05/11/2016','facile',20),
+('quest','05/11/2016','facile',20);
+
+INSERT INTO qcm_question values
+(1,1,'Informatique','BDD'),(1,2,'Informatique','BDD'),(1,3,'Informatique','BDD'),(1,4,'Informatique','PHP'),(1,5,'Informatique','PHP'),(1,6,'Informatique','PHP'),
+(2,7,'Informatique','JAVA'),(2,8,'Informatique','JAVA'),(2,9,'Informatique','JAVA'),(2,10,'Informatique','C'),(2,11,'Informatique','C'),(2,12,'Informatique','C'),
+(3,13,'Informatique','HTML'),(3,14,'Informatique','HTML'),(3,15,'Informatique','HTML'),(3,16,'Mathématiques','Algèbre'),(3,17,'Mathématiques','Algèbre'),(3,18,'Mathématiques','Algèbre'),
+(4,19,'Mathématiques','Géometrie'),(4,20,'Mathématiques','Géometrie'),(4,21,'Mathématiques','Géometrie'),(4,22,'Culture Générale','Histoire'),(4,23,'Culture Générale','Histoire'),(4,24,'Culture Générale','Histoire'),
+(5,25,'Culture Générale','Cinéma'),(5,26,'Culture Générale','Cinéma'),(5,27,'Culture Générale','Cinéma'),(5,28,'Médecine','Neurologie'),(5,29,'Médecine','Neurologie'),(5,30,'Médecine','Neurologie'),
+(6,31,'Médecine','Chirurgie'),(6,32,'Médecine','Chirurgie'),(6,33,'Médecine','Chirurgie'),(6,34,'Langues','Anglais'),(6,35,'Langues','Anglais'),(6,36,'Langues','Anglais'),
+(7,37,'Langues','Espagnol'),(7,38,'Langues','Espagnol'),(7,39,'Langues','Espagnol'),(7,40,'Physique Chimie','Physique'),(7,41,'Physique Chimie','Physique'),(7,42,'Physique Chimie','Physique'),
+(8,43,'Physique Chimie',NULL),(8,44,'Physique Chimie',NULL),(8,45,'Physique Chimie',NULL);
+
+
+
+
+
