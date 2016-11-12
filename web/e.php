5dc724de41c38b3829828027cc4b7584db236ce2
Fleetingshad <lucie.godefert42@laposte.net>
2016-11-12 18:41:54 +0100

    Répondeur : Exécution du QCM
    A raccorder avec s

--- web/e.php
+++ web/e.php
@@ -1,4 +1,4 @@
-  <?php 
+ <?php 
 try{
 $bdd=new PDO('pgsql:host=localhost;dbname=postgres','Lucie','2508028473F');
 }
@@ -11,6 +11,7 @@ catch(PDOException $e)
 $bdd->query('SET NAMES utf8');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
 try{
 
 if(isset($_GET['iq'])and trim($_GET['iq']!=' ')){
@@ -21,16 +22,18 @@ if(isset($_GET['iq'])and trim($_GET['iq']!=' ')){
 	
 	while($ligne=$req->fetch(PDO::FETCH_ASSOC))
 		{
-			echo '<p>'.$ligne['id_question'].'. '.$ligne['question'].'</p>';
+			echo ''.$ligne['id_question'].'. '.htmlspecialchars($ligne['question'],ENT_QUOTES).'</br>';
 	$req2=$bdd->prepare("SELECT * FROM public.reponse natural join public.question natural join qcm_question where id_qcm=:idqcm and id_question=:numeroquest"); 
 	$req2->bindValue(':idqcm',$_GET['iq']);
 	$req2->bindValue(':numeroquest',$ligne['id_question']);
 	$req2->execute();
+	echo'</br>';
 	while($l=$req2->fetch(PDO::FETCH_ASSOC))
 		{
-			echo '<form action="e.php" method="post"><input type="checkbox" name="reponse" value="'.$l['id_reponse'].'"/>'.$l['reponse'].'</form>';
+			echo '<form action="e.php" method="post"><input type="checkbox" name="reponse" value="'.$l['id_reponse'].'"/>'.htmlspecialchars($l['reponse'],ENT_QUOTES).'</form>';
 		
 		}
+		echo'</br>';
 		}
 	echo'</br><form action="s.php" method="post"><input type="submit" name="bouton" value="Valider"/></form></br>';
 	
