96a0db82466f5b7e5c1038412414d51ed6898893
Fleetingshad <lucie.godefert42@laposte.net>
2016-11-28 17:39:04 +0100

    Delete Creation.php

--- web/Creation.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<p>Import de questions</p>";
-echo "<a href='ImportQuestions.php'/><input type='button' value='Import de questions'/></a>";
-echo "<hr>";
-echo "<p>Création de questions</p>";
-echo "<a href='CreationQuestions.php'/><input type='button' value='Creation de questions'/></a>";
-?>
