71ec38716aa7452f63bd99a399ecab0ee8d0f3fd
Fleetingshad <lucie.godefert42@laposte.net>
2016-12-13 21:39:17 +0100

    Add files via upload

--- Web/Questions.php
+++ Web/Questions.php
@@ -27,7 +27,7 @@
     <h2 id="desk-hero"> Commencer à créer votre QCM</h2>
      <div id="menu-front">
 		 <ul>
-			 <li><a href="CreationQuestions.php">Créer une question</a></li>
+			 <li><a href="CreationQuestions.php?idd=$_GET['id']">Créer une question</a></li>
 			 <li><a href="Importer.php">Importer une question</a></li>
 		 </ul>
     </div>
@@ -35,7 +35,7 @@
 
 </header>
 
-<!-- END LANDING PAGE -->
+<!-- END LANDING PAGE --> 
 
 
 
@@ -51,36 +51,87 @@
 
     <p>
 <?php
-
+session_start();
 	require_once('Connexionbdd.php');
-
+echo var_dump($_POST);
 if (isset($_POST['q']))
 {
 	echo 'Question : '.$_POST['q'].'<br /><br />';
 
-	/*$req=$bdd->prepare('INSERT INTO public.question (question,valeur) VALUES (:q,:v)');
+	//inserer question
+	$req=$bdd->prepare('INSERT INTO question (question,valeur) VALUES (:q,:v)');
 	$req->bindValue(':q',$_POST['q']);
 	$req->bindValue(':v',$_POST['points']);
-	$req->execute();*/
+	$req->execute();
 
 	echo 'Réponses : <br /><br />';
 	$tab = array_combine($_POST['Rep'], $_POST['select']);
-
-
-	//$req2 = $bdd->prepare('INSERT INTO public.reponse(question,correct) VALUES(:r,:c)');
-	foreach($tab as $Rep => $VF)
-	{
-		if ($VF == 'Vrai')
-		{
-			echo $Rep.' -> Réponse Correcte<br/>';
-		}
-		elseif($VF == 'Faux')
-		{
-			echo $Rep.' -> Réponse Incorrecte<br/>';
-		}
+	
+	
+	//recuperer id question pour l'inserer dasn reponse
+	$idq=0;
+	$idquestion=$bdd->prepare('select id_question from question where question=:q');
+	$idquestion->bindValue(':q',$_POST['q']);
+	$idquestion->execute();
+	while($ligne=$idquestion->fetch(PDO::FETCH_ASSOC))
+    {
+		$idq=$ligne['id_question'];
 	}
-
+	
+	
+	$select;
+	foreach($_POST['select'] as $clee => $vall){
+		$select[]=$vall;
+	}
+	
+	
+	//inserer les reponses suivant faux/vrai
+	foreach($_POST['Rep'] as $cle => $val){
+	if($select[$cle]=='Vrai'){
+	$req2 = $bdd->prepare('INSERT INTO Reponse(id_question,reponse,correct) VALUES(:q,:rep,true)');
+	$req2->bindValue(':q',$idq);
+	$req2->bindValue(':rep',$val);
+	$req2->execute();
+	
+	}elseif($select[$cle]=='Faux'){
+	$req3 = $bdd->prepare('INSERT INTO reponse(id_question,reponse,correct) VALUES(:q,:rep,false)');
+	$req3->bindValue(':q',$idq);
+	$req3->bindValue(':rep',$val);
+	$req3->execute();
+	}
+	}
+	
 	echo '<br />Question à '.$_POST['points'].' points.';
+	
+	/////// C touche pas à ça, c'est la partie pour afficher : si tu veux tu peux essayer de recuperer l' id du qcm courant, vois avec N.
+	/*
+ 	$req=$bdd->prepare("SELECT * FROM qcm natural join qcm_question natural join question where id_qcm=:idqcm");
+	$req->bindValue(':idqcm',$_POST['id']);//
+	$req->execute();
+	while($ligne=$req->fetch(PDO::FETCH_ASSOC)){
+	echo 'Question : '.htmlspecialchars($ligne['question'],ENT_QUOTES).'</br>';
+	
+	
+	
+	$req2=$bdd->prepare("SELECT * FROM reponse  "); // natural join question natural join qcm_question natural join qcm where id_qcm=:idqcm and where id_question=:numeroquest
+	//$req2->bindValue(':idqcm',$_POST['id']);
+	//$req2->bindValue(':numeroquest',$ligne['id_question']);
+	$req2->execute();
+	echo'</br>';
+	while($l=$req2->fetch(PDO::FETCH_ASSOC)){
+			if($l['correct']){
+	echo '<div id=correct>Réponse : '.$l['reponse'].'</br></div>';
+	}else{
+	echo '<div id=false>Réponse : '.$l['reponse'].'</br></div>';
+	}
+		}
+	//}
+		echo'</br>';
+		*/
+	///////
+	
+	
+	
 }
 
 	if(isset($_POST['id']))
@@ -95,6 +146,10 @@ if (isset($_POST['q']))
 		}
 		echo 'id du qcm: '.$_POST['id'];
 	}
+	
+	
+	
+
 ?>
 </p>
 
