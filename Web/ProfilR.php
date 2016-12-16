1c7ed4a27eed4a7e04d9cda6192bab9563dbd95b
Fleetingshad <lucie.godefert42@laposte.net>
2016-12-16 10:19:31 +0100

    Add files via upload

---
+++ Web/ProfilR.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		 <link rel="stylesheet" href="ProfilR.css" />
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+    
+        
+        <div id="desk-nav">
+  <nav>
+    <ul>
+      <li><a href="AccueilR.php">Home</a></li>
+      <li><a href="ProfilR.php">Profil</a></li>
+      <li><a href="ChoixRD.php">QCM</a></li>
+      <li><a href="Index.php">Déconnexion</a></li>
+    </ul>
+  </nav>
+</div>
+        
+        
+        <?php 
+session_start();
+require_once('Connexionbdd.php');
+?>
+        
+        
+    <div class="material-wrap">
+<div class="material clearfix">
+	<div class="top-bar">
+		<div class="pull-left">
+			<a href="#" class="menu-tgl pull-left"><i class="fa fa-bars"></i></a>
+		</div>
+		<span class="title">Profil</span>
+		<div class="pull-right">
+			<a href="#" class="search-tgl pull-left"><i class="fa fa-search"></i></a>
+			<a href="#" class="option-tgl pull-left"><i class="fa fa-ellipsis-v"></i></a>
+		</div>
+	</div>
+	<div class="profile">
+		<div class="cover">
+			<span class="vec vec_a"></span>
+			<span class="vec vec_b"></span>
+			<span class="vec vec_c"></span>
+			<span class="vec vec_d"></span>
+			<span class="vec vec_e"></span>
+		</div>
+		<div class="photo">
+			<a href="" target="_blank"><img src="http://icons.iconarchive.com/icons/webalys/kameleon.pics/512/Alien-icon.png"></a>
+		</div>
+		<div class="info">
+			<div class="name">Pseudo</div>
+			<div class="position">Rang</div>
+		</div>
+		<a href="" onClick="javascript:window.history.go(-1)"> <input id="toggle" type="checkbox" class="plus"><label for="toggle" class="toggle"></label>
+		<div class="links">
+			<a href="" data-title="IDK"><i class="fa fa-facebook"></i></a>
+			<a href="" data-title="IDK"><i class="fa fa-twitter"></i></a>
+			<a href="" data-title="IDK"><i class="fa fa-codepen"></i></a>
+			<a href="" data-title="IDK"><i class="fa fa-pinterest"></i></a>
+		</div>
+            </a>
+	</div>
+    
+   
+    
+    <?php 
+$tim=0;
+$ins=$bdd->prepare('select * from repondeur where nom_repondeur=:n');
+$ins->bindValue(':n',$_SESSION['user']);
+$ins->execute();
+while($lu=$ins->fetch(PDO::FETCH_ASSOC)){
+		$tim=$lu['id_repondeur'];
+		}
+
+
+$req1 =$bdd->query("SELECT count(id_qcm) as somme FROM recap_repondeur WHERE id_repondeur = $tim");
+$o=$req1->fetch();
+$req1->closeCursor();
+$one=$o['somme'];
+
+$req2 =$bdd->query("SELECT round(avg(note_qcm)::numeric,2) as round FROM recap_repondeur WHERE id_repondeur = $tim");
+$t=$req2->fetch();
+$req2->closeCursor();
+$two=$t['round'];
+
+$req3 =$bdd->query("SELECT sum(temps_qcm) as sum FROM recap_repondeur WHERE id_repondeur = $tim");
+$th=$req3->fetch();
+$req3->closeCursor();
+$three=$th['sum'];
+
+
+$up1 =$bdd->prepare ('UPDATE repondeur SET nb_qcm_fait = :o WHERE id_repondeur = :id');
+$up1->bindValue(':id',$tim);
+$up1->bindValue(':o',$one);
+$up1->execute();
+
+$up2 =$bdd->prepare ('UPDATE repondeur SET moyenne = :t WHERE id_repondeur = :id');
+$up2->bindValue(':id',$tim);
+$up2->bindValue(':t',$two);
+$up2->execute();
+
+$up3 =$bdd->prepare ('UPDATE repondeur SET temps_total = :th WHERE id_repondeur = :id');
+$up3->bindValue(':id',$tim);
+$up3->bindValue(':th',$three);
+$up3->execute();
+		
+	
+$statrep =$bdd->prepare ('Select * from repondeur WHERE id_repondeur = :id');
+$statrep->bindValue(':id',$tim);
+$statrep->execute();
+while($l=$statrep->fetch(PDO::FETCH_ASSOC)){
+    echo '<div class="tabs clearfix"> <a href="#">';
+    
+    echo $l['temps_total'];    
+        
+    echo '</a><a href="#">';
+    echo $l['nb_qcm_fait'];
+    
+    echo '</a><a href="#">';
+    
+    echo $l['moyenne'];
+    
+    echo '</a></div>';
+}
+    ?>
+        
+	<div class="tabs-content">
+		<div class="friend-list">
+			<div class="list-ul">
+				<div class="list-li title">Récapitulatif</div>
+                
+    <?php
+    $d=$bdd->prepare('SELECT * FROM recap_repondeur WHERE id_repondeur = :id_rep');
+$d->bindValue(':id_rep',$tim);
+$d->execute();
+	while($l=$d->fetch(PDO::FETCH_ASSOC)){
+                
+				
+            echo'    <div class="list-li clearfix">	
+					<div class="info pull-left">
+						<div class="name">';
+              
+                echo $l['domaine']. $l['sous_domaine'];
+                
+                echo'</div>
+						<div class="time">';
+                    
+                    echo $l['date_qcm_fait'];
+        
+       echo' </div>
+					</div>
+					<div class="action pull-right">
+						<div class="name">';
+           echo $l['note_qcm'];
+        
+        echo'</div>
+						<div class="time">';
+        echo $l['temps_qcm'];
+        
+        echo'</div>
+					</div>
+				</div>';
+    }
+                                
+        ?>        
+	
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+    
+    
+    </body>
+	</html>
+    
+    
+    
---
+++ Web/Statistique.php
@@ -0,0 +1,176 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		 <link rel="stylesheet" href="Resultat.css" />
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+        <title></title>
+    </head>
+    <body> 
+
+
+        
+        <div id="desk-nav">
+  <nav>
+    <ul>
+      <li><a href="AccueilR.php">Home</a></li>
+      <li><a href="ProfilR.php">Profil</a></li>
+      <li><a href="ChoixRD.php">QCM</a></li>
+      <li><a href="Index.php">Déconnexion</a></li>
+    </ul>
+  </nav>
+</div>
+        
+        
+        
+        
+        
+<?php require_once('Connexionbdd.php'); ?>
+
+<div class="container">
+    <h1>Résultat</h1>
+    
+    <?php 
+session_start();
+try{
+
+require_once('Connexionbdd.php');
+$date=time().'</br>';
+$tempspasse=$date-$_POST['temps'];
+$score=0;$faux=0;$vrai=0;
+if(isset($_POST['reponse'])and trim($_POST['reponse']!=' ')){
+	if(isset($_POST['checkboxes'])and trim($_POST['checkboxes']!=' ')){
+		echo '<h2>Vos réponses au QCM n° '.$_POST['qcm'].' : </h2> ';
+        
+        $question=$bdd->prepare('Select * from qcm natural join public.qcm_question natural join question where id_qcm=:idqcm');//pour chaque question
+		$question->bindValue(':idqcm',$_POST['qcm']);
+		$question->execute();
+		while($quest=$question->fetch(PDO :: FETCH_ASSOC)){
+        
+            echo "<div class=\"form-group\"><label class=\"control-label\" for=\"select\">";
+			echo 'Question : '.$quest['question'];		//affichage question
+            echo "</label><i class=\"bar\"></i></div> ";
+            
+              $idquestionn=$quest['id_question'];
+			$reponse=$bdd->prepare('Select * from public.reponse natural join public.qcm_question where id_question=:idquestion and id_qcm=:idqcm');//pour chaque réponse de la question
+			$reponse->bindValue(':idquestion',$idquestionn);
+            $reponse->bindValue(':idqcm',$_POST['qcm']);
+			$reponse->execute();
+			while($rep=$reponse->fetch(PDO::FETCH_ASSOC)){
+				foreach($_POST['reponse'] as $c=>$v){
+					
+					
+					if($rep['id_reponse']==$v){
+                    
+        
+         echo'<i class="helper"></i><i class="helper"></i></br> Votre réponse: '.htmlspecialchars($rep['reponse'],ENT_QUOTES);
+       
+        
+                    if ($rep['correct']){
+                        
+         echo'<i class="helper"><div class="green">Réponse juste</i></div> ';
+        
+                           $vrai+=1;
+							}else{
+								echo'<i class="helper"><div class="red">Réponse fausse</i></div> </br>';	//si la réponse est fausse
+								echo 'La réponse juste était :' ;
+								$repjuste=$bdd->prepare('Select * from public.question natural join public.reponse natural join qcm where correct=TRUE and id_question=:mq and id_qcm=:idqcm');//trouve la réponse juste
+								$repjuste->bindValue(':mq',$idquestionn);
+                                $repjuste->bindValue(':idqcm',$_POST['qcm']);
+								$repjuste->execute();
+								while($l=$repjuste->fetch(PDO :: FETCH_ASSOC)){
+									echo' '.htmlspecialchars($l['reponse'],ENT_QUOTES);//affichage réponse juste
+								}
+								$faux=1;
+							}
+					}
+				}
+			}
+			if($faux==0 && $vrai==1){
+				$point=$bdd->prepare('Select * from question where id_question=:mq');
+								$point->bindValue(':mq',$idquestionn);
+								$point->execute();
+								while($ligne=$point->fetch(PDO::FETCH_ASSOC)){
+										$score+=$ligne['valeur'];
+										echo '</br>Score : + '.$ligne['valeur'].'</br>';
+								}
+			}
+			$faux=0;
+			$vrai=0;
+		}
+	echo'</br></br></br>';
+	
+	$time=0;
+	$tempsdepasse=$bdd->prepare('Select * FROM public.qcm_question natural join public.question where id_qcm=:idqcm');
+	$tempsdepasse->bindValue(':idqcm',$_POST['qcm']);
+	$tempsdepasse->execute();
+	$t=0;
+	while($l=$tempsdepasse->fetch(PDO::FETCH_ASSOC)){
+		$t+=$l['temps'];
+	}
+	if($tempspasse>$t){
+		$score-=1;
+		echo 'Vous avez dépassé le temps (score - 1) : '.$tempspasse.' secondes. </br>';
+	}
+	if($score<0){
+		$score=0;
+	}if ($tempspasse<=$t){
+        
+	echo 'Temps passé : '.$tempspasse.' secondes.</br>';
+	}
+	echo ' Score : '.$score.'</br></br>';
+	}
+}
+
+$tim=0;
+$ins=$bdd->prepare('select * from repondeur where nom_repondeur=:n');
+$ins->bindValue(':n',$_SESSION['user']);
+$ins->execute();
+while($lu=$ins->fetch(PDO::FETCH_ASSOC)){
+		$tim=$lu['id_repondeur'];
+		}
+
+
+$dom=0;$s_dom=0;
+$d_sd=$bdd->prepare('SELECT distinct domaine,sous_domaine FROM qcm_question natural join qcm WHERE id_qcm = :id_qcm');
+$d_sd->bindValue(':id_qcm',$_POST['qcm']);
+$d_sd->execute();
+	while($l=$d_sd->fetch(PDO::FETCH_ASSOC)){
+		$dom=$l['domaine'];
+		$s_dom=$l['sous_domaine'];
+	}
+
+
+$d='seconds';
+$inserer=$bdd->prepare('insert into public.recap_repondeur (id_repondeur,id_qcm,domaine,sous_domaine,date_qcm_fait,note_qcm,temps_qcm) values(:id_rep,:id_qcm,:dom,:s_dom,date_trunc(:mot,now()),:note,:tempspasse)');
+$inserer->bindValue(':id_rep',$tim);
+$inserer->bindValue(':id_qcm',$_POST['qcm']);
+$inserer->bindValue(':dom',$dom);
+$inserer->bindValue(':s_dom',$s_dom);
+$inserer->bindValue(':mot',$d);
+$inserer->bindValue(':note',$score);
+$inserer->bindValue(':tempspasse',$tempspasse);
+$inserer->execute();
+}
+
+catch(PDOException $e){
+	die('<p>Votre requête est erronée.</p>'.$e);
+}
+	
+	 echo '<div class="button-container">
+    <a href="ProfilR.php"><button class="button" type="submit"><span>IDK</span></button></a>
+  </div>';
+    
+     echo "</form>";
+?>
+
+
+  
+</div>
+    
+    </body>
+	</html>
+            
+            
+            
