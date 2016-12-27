f3f914d7a7de88e7f5fea669c8834e464a0d5ee5
Fleetingshad <lucie.godefert42@laposte.net>
2016-12-27 18:17:04 +0100

    Résultat par dom/sdom

--- Web/ProfilR.php
+++ Web/ProfilR.php
@@ -131,40 +131,11 @@ while($l=$statrep->fetch(PDO::FETCH_ASSOC)){
 				<div class="list-li title">Récapitulatif</div>
                 
     <?php
-	///////////////////
-if(isset($_GET['d'])and trim ($_GET['d']!='') and !isset($_GET['sd'])){
-	//  where recap_repondeur.domaine=domaine.domaine and sous_domaine.id_domaine=domaine.id_domaine and sous_domaine.id_domaine=:d
-	$req=$bdd->prepare("SELECT distinct domaine,sous_domaine FROM recap_repondeur natural join sous_domaine natural join domaine where id_domaine=:d");
-	$req->bindValue(':d',$_GET['d']);
-	$req->execute();
-	while($ligne=$req->fetch(PDO::FETCH_ASSOC))
-	{
-        echo'    <div class="list-li clearfix">	
-					<div class="info pull-left">
-						<div class="name">';
-		echo '<a href="ProfilR.php?d='.$ligne['domaine'].'&sd='.$ligne['sous_domaine'].'">'.$ligne['sous_domaine'].'</a>';
-        echo'</div>
-					</div>
-				</div>';
-	}
-}else{	 
-	$dom=$bdd->prepare('SELECT distinct id_domaine,domaine FROM domaine natural join recap_repondeur');
-	$dom->execute();
-	while($li=$dom->fetch(PDO::FETCH_ASSOC)){
-		echo '
-		<div class="list-li clearfix">	
-					<div class="info pull-left">
-						<div class="name">';
-              echo '<a href="ProfilR.php?d='.$li['id_domaine'].'">'.$li['domaine'].'</a>';
-                
-                echo'</div></div></div>';
-	}
-}
-	/////////////////////
-	/*
 	
-    $d=$bdd->prepare('SELECT * FROM recap_repondeur WHERE id_repondeur = :id_rep');
+if(isset($_GET['d'])and trim ($_GET['d']!='') and isset($_GET['sd']) and trim ($_GET['sd']!='') ){
+$d=$bdd->prepare('SELECT * FROM recap_repondeur WHERE id_repondeur = :id_rep and sous_domaine=:sd');
 $d->bindValue(':id_rep',$tim);
+$d->bindValue(':sd',$_GET['sd']);
 $d->execute();
 	while($l=$d->fetch(PDO::FETCH_ASSOC)){
                 
@@ -173,7 +144,7 @@ $d->execute();
 					<div class="info pull-left">
 						<div class="name">';
               
-                echo $l['domaine']. $l['sous_domaine'];
+                echo $l['domaine'].' '.$l['sous_domaine'];
                 
                 echo'</div>
 						<div class="time">';
@@ -193,7 +164,37 @@ $d->execute();
         echo'</div>
 					</div>
 				</div>';
-    }*/
+    }
+
+}else if(isset($_GET['d'])and trim ($_GET['d']!='') and !isset($_GET['sd'])){
+	$req=$bdd->prepare("SELECT distinct domaine,sous_domaine FROM recap_repondeur natural join sous_domaine natural join domaine where id_domaine=:d and id_repondeur=:id_rep");
+	$req->bindValue(':d',$_GET['d']);
+	$req->bindValue(':id_rep',$tim);
+	$req->execute();
+	while($ligne=$req->fetch(PDO::FETCH_ASSOC))
+	{
+        echo'    <div class="list-li clearfix">	
+					<div class="info pull-left">
+						<div class="name">';
+		echo '<a href="ProfilR.php?d='.$ligne['domaine'].'&sd='.$ligne['sous_domaine'].'">'.$ligne['sous_domaine'].'</a>';
+        echo'</div>
+					</div>
+				</div>';
+	}
+}else{	 
+	$dom=$bdd->prepare('SELECT distinct id_domaine,domaine FROM domaine natural join recap_repondeur where id_repondeur = :id_rep ');
+	$dom->bindValue(':id_rep',$tim);
+	$dom->execute();
+	while($li=$dom->fetch(PDO::FETCH_ASSOC)){
+		echo '
+		<div class="list-li clearfix">	
+					<div class="info pull-left">
+						<div class="name">';
+              echo '<a href="ProfilR.php?d='.$li['id_domaine'].'">'.$li['domaine'].'</a>';
+                
+                echo'</div></div></div>';
+	}
+}
                                 
         ?>        
 	
