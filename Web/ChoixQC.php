3d4f98119833602f1ac32a0ae432d6807aa1b173
Fleetingshad <lucie.godefert42@laposte.net>
2017-01-11 13:20:00 +0100

    Créer domaine fonctionne !

--- Web/ChoixQC.php
+++ Web/ChoixQC.php
@@ -66,38 +66,41 @@ if(isset($_GET['d']) and trim($_GET['d']!='')and !(isset($_GET['sd'])))//domaine
 }
 else if (isset($_GET['sd']) and trim($_GET['sd']!='') and isset($_GET['d']) and trim($_GET['d']!=''))//sous domaine sélectionné,création du qcm avec valeurs par defaut et id attribué.
 {
+	
+	
 	if($_GET['sd']=='general'){
-	$req=$bdd->prepare("INSERT into qcm(auteur,domaine) values(:quest,:d)");
+	$req=$bdd->prepare("INSERT into qcm(auteur,domaine,note_total) values(:quest,:d,0)");
 	$req->bindValue(':quest',$_SESSION['user']);
 	$req->bindValue(':d',$_GET['d']);
 	$req->execute();
 	}else{
-	$req=$bdd->prepare("INSERT into qcm(auteur,domaine,sous_domaine) values(:quest,:d,:sd)");
+	
+	$req=$bdd->prepare("INSERT into qcm(auteur,domaine,sous_domaine,note_total) values(:quest,:d,:sd,0)");
 	$req->bindValue(':quest',$_SESSION['user']);
 	$req->bindValue(':d',$_GET['d']);
 	$req->bindValue(':sd',$_GET['sd']);
 	$req->execute();	
 	}
 	
+	
 	$req=$bdd->prepare("SELECT * from qcm");
 	$req->execute();
 	while($ligne=$req->fetch(PDO::FETCH_ASSOC)){
 		$id=$ligne['id_qcm'];
 		}
+		
 	
 	echo '<p>Vous allez maintenant pouvoir créer votre qcm</p>';
 	if($_GET['sd']=='general')
 	{
          
-		echo '<form action="ChoixQQ.php" method="post"><input type="hidden" name="id" value="'.$id.'"/><input type="hidden" name="dom" value="'.$_GET['d'].'"/><button type="submit"class="start1"/>Commencer le QCM</button></form>';
+		echo '<form action="ChoixQQ.php" method="post"><input type="hidden" name="id" value="'.$id.'"/><input type="hidden" name="dom" value="'.$_GET['d'].'"/><button type="submit"class="start"/>Commencer le QCM</button></form>';
         
 	}
 	else
 	{
-         
-		echo '<form action="ChoixQQ.php" method="post"><input type="hidden" name="id" value="'.$id.'"/><input type="hidden" name="dom" value="'.$_GET['d'].'"/><input type="hidden" name="sdom" value="'.$_GET['sd'].'"/><button type="submit"class="start1"/>Commencer le QCM</button></form>';
-        
-        
+		echo '<form action="ChoixQQ.php" method="post"><input type="hidden" name="id" value="'.$id.'"/><input type="hidden" name="dom" value="'.$_GET['d'].'"/><input type="hidden" name="sdom" value="'.$_GET['sd'].'"/><button type="submit"class="start"/>Commencer le QCM</button></form>';
+		
 	}
 }//redirection vers Questions.php avec le domaine,l'id du qcm  et le sous domaione du qcm en $_post.si domaine général le sous domaine n'est pas transmis
 else//1-entrée de la création du qcm,affichage des domaines de la bdd
@@ -115,11 +118,9 @@ else//1-entrée de la création du qcm,affichage des domaines de la bdd
         echo "</div></div>";
 	}
 }
-
 ?>            
       </div>
-
       <div class="rela-block button black-text load-button"><a href="" onClick="javascript:window.history.go(-1)">Retour</a></div>
-            
-	</body>
+       
+</body>
 </html>
