3f4e522c781bf5c6b513186d3aca4e054af0c835
Skeldrarg <claudetab59@gmail.com>
2017-03-28 13:56:47 +0200

    Modifs

--- Web/Questionneur/CreationQuestions.php
+++ Web/Questionneur/CreationQuestions.php
@@ -59,9 +59,15 @@ if (isset($_POST['idd']) and trim($_POST['idd']!='') and isset($_POST['desc']) a
 					</select>
 				</span>
 			</div>
+			
+<?php
+			echo '<input type="hidden" name="id" value="'.$_POST['idqcm'].'"/>';
+?>
+			<div class="message">
+				<textarea placeholder="Rajouter un petit commentaire ici. Cela sera affiché sous votre question. "></textarea>
+			</div>
 			<div class="ajouter">
-                
-				<input type="button" id="ajouterChamp" value="Ajouter une réponse" />
+				<input type="button" id="ajouterChamp" value="Ajouter une réponse"/>
 			</div>
 			<script>
 				$(document).ready(function()
@@ -74,10 +80,6 @@ if (isset($_POST['idd']) and trim($_POST['idd']!='') and isset($_POST['desc']) a
 					});
 				});
 			</script>
-<?php
-			echo '<input type="hidden" name="id"  value="'.$_POST['idqcm'].'" />';
-?>
-			
 			<input type="hidden" name="creaquestion" value="creaquestion"/>
 			<footer>
 				<span class="attention">Tous les champs doivent être remplis. Les champs de temps et de score ne doivent contenir que des nombres</span>
